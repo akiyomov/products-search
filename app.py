6a5042b55b1b31f0b1c1d7918068313c20c6ad99
akiyomov <114338623+akiyomov@users.noreply.github.com>
2024-01-10 11:52:03 +0900

    deployment update

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_product_info():
         return jsonify({'error': 'Product information not found for the given barcode'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
