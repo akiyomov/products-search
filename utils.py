cb2ede034f89b375f3d3bd5cf3e46b493c22c2cb
akiyomov <114338623+akiyomov@users.noreply.github.com>
2024-01-16 17:15:57 +0900

    updated json returing

--- utils.py
+++ utils.py
@@ -1,16 +1,29 @@
-import json
 import pandas as pd
+import requests
+
+def image_exists(bucket_url):
+    """
+    Checks if the image exists in the specified bucket using an HTTP GET request.
+
+    Args:
+    - bucket_url (str): The URL of the image in the bucket.
+
+    Returns:
+    - bool: True if the image exists, False otherwise.
+    """
+    response = requests.head(bucket_url)
+    return response.status_code == 200
 
 def get_by_barcode(df, barcode):
     """
     Returns product information from the given DataFrame that matches the given barcode.
-    
+
     Args:
     - df (DataFrame): The DataFrame to search.
     - barcode (str or int): The barcode to search for.
-    
+
     Returns:
-    - str: Product information matching the given barcode in JSON format.
+    - dict: Product information matching the given barcode.
     """
     if not isinstance(barcode, int):
         if str(barcode).isdigit():
@@ -19,7 +32,18 @@ def get_by_barcode(df, barcode):
     filtered_row = df[df["barcode"] == barcode][columns_to_select]
     if len(filtered_row) > 0:
         product_info = filtered_row.iloc[0].to_dict()
+
+        # Replace NaN values with None
+        product_info = {key: value if pd.notna(value) else None for key, value in product_info.items()}
+
+        # Construct image URL and check if the image exists
+        image_url = f'https://storage.googleapis.com/product-search-bds/images/{barcode}.jpg'
+        if image_exists(image_url):
+            product_info['image'] = image_url
+        else:
+            # If the image can't be found, use the default image URL
+            product_info['image'] = 'https://storage.googleapis.com/product-search-bds/images/default.png'
+
         return product_info
     else:
-        return None  # Return None if barcode is not found in the DataFrame or DataFrame is empty
-
+        return None  # Return None if the barcode is not found in the DataFrame or DataFrame is empty
