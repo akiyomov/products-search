f709aac7779bf1b52916044885d79fe683969dea
Asadbek <asadbey0331@gmail.com>
2024-02-18 19:07:04 +0900

    optimezed return

--- backend/app.py
+++ backend/app.py
@@ -3,18 +3,32 @@ from backend.utils import ProductDatabase
 from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
-product_db = ProductDatabase('backend/sample.csv')  # Update with the actual path to your CSV file
+product_db = ProductDatabase('sample.csv')  # Ensure this path points to your actual CSV file
 
 @app.route('/get_product_info', methods=['GET'])
 def get_product_info():
     barcode = request.args.get('barcode')
     language = request.args.get('language', 'en')  # Default to English
-    print(barcode,language)
+
     if not barcode or not ProductDatabase.validate_barcode(barcode):
         raise BadRequest('Invalid or missing barcode parameter')
 
-    product_info = product_db.get_by_barcode(barcode, language)
-    if product_info:
+    product = product_db.get_by_barcode(barcode, language)
+    if product:
+        # Serialize the Product object to a JSON-friendly format
+        product_info = {
+            "barcode": product.barcode,
+            "company": product.company if product.company else None,
+            "product": product.product if product.product else None,
+            "type": product.product_type if product.product_type else None,
+            "package": product.packaging if product.packaging else None,
+            "volume_ml": product.volume_ml if product.volume_ml else None,
+            "country": product.country if product.country else None,
+            "boycott": product.boycott if product.boycott else None,
+            "certificate": product.certificate if product.certificate else None,
+            "image_url": product.image_url if product.image_url else None,
+            "boycott_reason": product.boycott_reason if product.boycott_reason else None,
+        }
         return jsonify({'product_info': product_info})
     else:
         return jsonify({'error': 'Product information not found for the given barcode'}), 404
@@ -24,4 +38,4 @@ def handle_bad_request(e):
     return jsonify({'error': 'Bad request', 'message': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, port=8010)
